bd797279518b1f4908b7eacd22319303d83e0999
Brain <brainnovocaine@msn.com>
2018-10-09 13:02:32 -0600

    Fourth

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3040;
+var PORT = process.env.PORT || 3042;
 
 // Initialize Express
 var app = express();
@@ -155,6 +155,6 @@ app.post("/articles/:id", function(req, res) {
 });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 3042, function() {
   console.log("App running on port " + PORT + "!");
 });
