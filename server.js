9071042f640bac0be6bea659ade3f99e6439d042
Brain <brainnovocaine@msn.com>
2018-10-09 22:50:38 -0600

    sixthAttempt

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+var MongoClient = require("mongodb");
 var mongoose = require("mongoose");
 var path = require("path");
 // Our scraping tools
+
 // Axios is a promised-based http library, similar to jQuery's Ajax method. Axios is similiar to 'request'. It works with REACT
 // It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 // cheerio runs ontop of jQUERY - it basically is modified JQUERY - its used to grab content so that you can use: each, child and parent to manipulate elements you scrape from webpages
-
+var option = {
+  socketTimeoutMS: 30000,
+  keepAlive: true,
+  reconnectTries: 30000
+};
 // Require all models
 var db = require("./models");
 
@@ -31,16 +37,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/ScraperScrapper";
+// mongodb://localhost:27017/db/chat
+// var MONGODB_URI =
+//   process.env.MONGODB_URI || "mongod://localhost/:2707/ScraperScrapper", ({ useNewUrlParser: true });
+
+mongoose.Promise = Promise;
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost/scraper",
+    // "mongodb://myUsername:myPassword@ds161039.mlab.com:61039/accounttest"
+    { useNewUrlParser: true }
+  )
+  .then(result => {
+    console.log("connected!");
+  })
+  .catch(err => {
+    console.log("error", err);
+  });
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect(databaseUri);
+// }
+var dv = mongoose.connection;
+
+dv.on("error", function(err) {
+  console.log("Mongoose Error!", err);
+});
+dv.once("open", function() {
+  console.log("Mongoose Successfully Loaded!");
+});
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(
-  MONGODB_URI,
-  { useNewUrlParser: true }
-);
+
 // var routes = require("./controllers/burgersController.js");
 
 // app.use("/", routes);
@@ -168,3 +198,5 @@ app.post("/articles/:id", function(req, res) {
 app.listen(process.env.PORT || 3042, function() {
   console.log("App running on port " + PORT + "!");
 });
+
+// HOMEWORK, RE-DOING ACTIVITIES, WATCHING CLASS VIDEOS, ADDITIONAL LEARNING
